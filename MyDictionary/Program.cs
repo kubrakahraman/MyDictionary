2765e88aae68798ac5364a58909c7a20d34078a8
Kübra kahraman <fatma_hamzadayioglu@msn.com>
2021-02-08 20:51:28 +0300

    Add project files.

---
+++ MyDictionary/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace MyDictionary
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            MyDictionary<int, string> dersler = new MyDictionary<int, string>();
+            dersler.Add(1, "Matematik");
+            dersler.Add(2, "Türkçe");
+            dersler.Add(3, "Fizik");
+
+        }
+    }
+}
